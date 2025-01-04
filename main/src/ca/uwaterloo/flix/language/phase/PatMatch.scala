008f4f20d8721e672328ec6566725f3793b3cb2e
Matthew Lutze <mlutze@users.noreply.github.com>
2025-01-04 12:10:11 +0100

    refactor use Types in PatMatch

--- main/src/ca/uwaterloo/flix/language/phase/PatMatch.scala
+++ main/src/ca/uwaterloo/flix/language/phase/PatMatch.scala
@@ -18,7 +18,7 @@ package ca.uwaterloo.flix.language.phase
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.*
-import ca.uwaterloo.flix.language.ast.TypedAst.{Expr, ParYieldFragment, Pattern, Root}
+import ca.uwaterloo.flix.language.ast.TypedAst.{Case, Expr, ParYieldFragment, Pattern, Root}
 import ca.uwaterloo.flix.language.ast.ops.TypedAstOps
 import ca.uwaterloo.flix.language.ast.shared.Constant
 import ca.uwaterloo.flix.language.ast.shared.SymUse.CaseSymUse
@@ -251,7 +251,7 @@ object PatMatch {
     */
   private def checkFrags(frags: List[ParYieldFragment], root: TypedAst.Root, loc: SourceLocation): List[NonExhaustiveMatchError] = {
     // Call findNonMatchingPat for each pattern individually
-    frags.flatMap(f => findNonMatchingPat(List(List(f.pat)), 1, root) match {
+    frags.flatMap(f => findNonMatchingPat(List(List(f.pat)), 1, f.exp.tpe, root) match {
       case Exhaustive => Nil
       case NonExhaustive(ctors) => NonExhaustiveMatchError(prettyPrintCtor(ctors.head), loc) :: Nil
     })
@@ -269,7 +269,7 @@ object PatMatch {
     // Filter down to the unguarded rules.
     // Guarded rules cannot contribute to exhaustiveness (the guard could be e.g. `false`)
     val unguardedRules = rules.filter(r => r.guard.isEmpty)
-    findNonMatchingPat(unguardedRules.map(r => List(r.pat)), 1, root) match {
+    findNonMatchingPat(unguardedRules.map(r => List(r.pat)), 1, exp.tpe, root) match {
       case Exhaustive => Nil
       case NonExhaustive(ctors) => List(NonExhaustiveMatchError(prettyPrintCtor(ctors.head), exp.loc))
     }
@@ -284,7 +284,7 @@ object PatMatch {
     * @param root  The AST root of the expression
     * @return If no such pattern exists, returns Exhaustive, else returns NonExhaustive(a matching pattern)
     */
-  private def findNonMatchingPat(rules: List[List[Pattern]], n: Int, root: TypedAst.Root): Exhaustiveness = {
+  private def findNonMatchingPat(rules: List[List[Pattern]], n: Int, tpe: Type, root: TypedAst.Root): Exhaustiveness = {
     if (n == 0) {
       if (rules.isEmpty) {
         return NonExhaustive(List.empty[TyCon])
@@ -293,7 +293,7 @@ object PatMatch {
     }
 
     val sigma = rootCtors(rules)
-    val missing = missingFromSig(sigma, root)
+    val missing = missingFromSig(sigma, tpe, root)
     if (missing.isEmpty && sigma.nonEmpty) {
       /* If the constructors are complete, then we check that the arguments to the constructors and the remaining
        * patterns are complete
@@ -313,7 +313,7 @@ object PatMatch {
        * exhaustive. So we create a "Specialized" matrix for "Some" with {True, False} as rows and check that.
        */
       val checkAll: List[Exhaustiveness] = sigma.map(c => {
-        val res: Exhaustiveness = findNonMatchingPat(specialize(c, rules, root), countCtorArgs(c) + n - 1, root)
+        val res: Exhaustiveness = findNonMatchingPat(specialize(c, rules, root), countCtorArgs(c) + n - 1, ??? /* MATT what type here? */, root)
         res match {
           case Exhaustive => Exhaustive
           case NonExhaustive(ctors) => NonExhaustive(rebuildPattern(c, ctors))
@@ -325,7 +325,7 @@ object PatMatch {
       /* If the constructors are not complete, then we will fall to the wild/default case. In that case, we need to
        * check for non matching patterns in the wild/default matrix.
        */
-      findNonMatchingPat(defaultMatrix(rules), n - 1, root) match {
+      findNonMatchingPat(defaultMatrix(rules), n - 1, ??? /* MATT what type here? */, root) match {
         case Exhaustive => Exhaustive
         case NonExhaustive(ctors) => sigma match {
           // If sigma is not empty, pick one of the missing constructors and return it
@@ -496,43 +496,101 @@ object PatMatch {
     * Wildcards are exhaustive, but we need to do some additional checking in that case (@see defaultMatrix)
     *
     * @param ctors The ctors that we match with
+    * @param tpe MATT todo
     * @param root  Root of the expression tree
     * @return
     */
-  private def missingFromSig(ctors: List[TyCon], root: TypedAst.Root): List[TyCon] = {
-    // Enumerate all the constructors that we need to cover
-    def getAllCtors(x: TyCon): List[TyCon] = x match {
-      // Structural types have just one constructor
-      case a: TyCon.Tuple => List(a)
-      case a: TyCon.Record => List(a)
-
-      // For Enums, we have to figure out what base enum is, then look it up in the enum definitions to get the
-      // other cases
-      case TyCon.Enum(sym, _) => {
-        root.enums(sym.enumSym).cases.map {
-          case (otherSym, caze) => TyCon.Enum(otherSym, List.fill(caze.tpes.length)(TyCon.Wild))
+  private def missingFromSig(ctors: List[TyCon], tpe: Type, root: TypedAst.Root): List[TyCon] = {
+    enumerateConstructors(tpe, root) match {
+      // If constructors are finite, then we check that they are all there
+      case Constructors.Finite(expCtors) =>
+        expCtors.filterNot {
+          ctor => ctors.exists(y => sameCtor(ctor, y))
         }
-      }.toList
 
-      // For Unit and Bool constants are enumerable
-      case TyCon.Cst(Constant.Unit) => List(TyCon.Cst(Constant.Unit))
-      case TyCon.Cst(Constant.Bool(_)) => List(TyCon.Cst(Constant.Bool(true)), TyCon.Cst(Constant.Bool(false)))
-
-      /* For numeric types, we consider them as "infinite" types union
-       * Int = ...| -1 | 0 | 1 | 2 | 3 | ...
-       * The only way we get a match is through a wild. Technically, you could, for example, cover a Char by
-       * having a case for [0 255], but we'll ignore that case for now
-       */
-      case _ => List(TyCon.Wild)
+      // If constructors are infinite, then we must have a wild case
+      case Constructors.Infinite =>
+        if (ctors.contains(TyCon.Wild)) {
+          Nil
+        } else {
+          List(TyCon.Wild)
+        }
     }
+  }
 
-    val expCtors = ctors.flatMap(getAllCtors)
-    /* We cover the needed constructors if there is a wild card in the
-     * root constructor set, or if we match every constructor for the
-     * expression
-     */
-    expCtors.filterNot {
-      ctor => ctors.exists(y => sameCtor(ctor, y))
+  private def enumerateConstructors(tpe: Type, root: Root): Constructors = {
+    tpe.typeConstructor match {
+      case None => Constructors.Infinite
+      case Some(cst) => cst match {
+
+        case TypeConstructor.Void => Constructors.Finite(Nil)
+        case TypeConstructor.Unit => Constructors.Finite(List(TyCon.Cst(Constant.Unit)))
+        case TypeConstructor.Null => Constructors.Finite(List(TyCon.Cst(Constant.Null)))
+        case TypeConstructor.Bool => Constructors.Finite(List(TyCon.Cst(Constant.Bool(true)), TyCon.Cst(Constant.Bool(false))))
+        case TypeConstructor.Char => Constructors.Infinite
+        case TypeConstructor.Float32 => Constructors.Infinite
+        case TypeConstructor.Float64 => Constructors.Infinite
+        case TypeConstructor.BigDecimal => Constructors.Infinite
+        case TypeConstructor.Int8 => Constructors.Infinite
+        case TypeConstructor.Int16 => Constructors.Infinite
+        case TypeConstructor.Int32 => Constructors.Infinite
+        case TypeConstructor.Int64 => Constructors.Infinite
+        case TypeConstructor.BigInt => Constructors.Infinite
+        case TypeConstructor.Str => Constructors.Infinite
+        case TypeConstructor.Regex => Constructors.Infinite
+        case TypeConstructor.Arrow(arity) => Constructors.Infinite
+        case TypeConstructor.Record => Constructors.Infinite
+        case TypeConstructor.Schema => Constructors.Infinite
+        case TypeConstructor.Sender => Constructors.Infinite
+        case TypeConstructor.Receiver => Constructors.Infinite
+        case TypeConstructor.Lazy => Constructors.Infinite
+        case TypeConstructor.Enum(sym, kind) =>
+          val tycons = root.enums(sym).cases.map {
+            case (caseSym, Case(_, tpes, _, _)) => TyCon.Enum(caseSym, tpes.map(_ => TyCon.Wild))
+          }
+          Constructors.Finite(tycons.toList)
+        case TypeConstructor.Struct(sym, kind) => Constructors.Infinite
+        case TypeConstructor.RestrictableEnum(sym, kind) => Constructors.Infinite
+        case TypeConstructor.Native(clazz) => Constructors.Infinite
+        case TypeConstructor.Array => Constructors.Infinite
+        case TypeConstructor.Vector => Constructors.Infinite
+        case TypeConstructor.Tuple(l) => Constructors.Finite(List(TyCon.Tuple(List.fill(l)(TyCon.Wild))))
+        case TypeConstructor.RegionToStar => Constructors.Infinite
+        case TypeConstructor.Error(id, kind) => Constructors.Infinite
+
+        // MATT throw ICE
+        case TypeConstructor.AnyType => ???
+        case TypeConstructor.ArrowWithoutEffect(arity) => ???
+        case TypeConstructor.RecordRowEmpty => ???
+        case TypeConstructor.RecordRowExtend(label) => ???
+        case TypeConstructor.SchemaRowEmpty => ???
+        case TypeConstructor.SchemaRowExtend(pred) => ???
+        case TypeConstructor.JvmConstructor(constructor) => ???
+        case TypeConstructor.JvmMethod(method) => ???
+        case TypeConstructor.JvmField(field) => ???
+        case TypeConstructor.ArrayWithoutRegion => ???
+        case TypeConstructor.Relation => ???
+        case TypeConstructor.Lattice => ???
+        case TypeConstructor.True => ???
+        case TypeConstructor.False => ???
+        case TypeConstructor.Not => ???
+        case TypeConstructor.And => ???
+        case TypeConstructor.Or => ???
+        case TypeConstructor.Pure => ???
+        case TypeConstructor.Univ => ???
+        case TypeConstructor.Complement => ???
+        case TypeConstructor.Union => ???
+        case TypeConstructor.Intersection => ???
+        case TypeConstructor.Difference => ???
+        case TypeConstructor.SymmetricDiff => ???
+        case TypeConstructor.Effect(sym) => ???
+        case TypeConstructor.CaseComplement(sym) => ???
+        case TypeConstructor.CaseUnion(sym) => ???
+        case TypeConstructor.CaseIntersection(sym) => ???
+        case TypeConstructor.CaseSet(syms, enumSym) => ???
+        case TypeConstructor.RegionWithoutRegion => ???
+      }
+
     }
   }
 
@@ -666,5 +724,15 @@ object PatMatch {
     */
   private def mergeAllExhaustive(l: List[Exhaustiveness]): Exhaustiveness = {
     l.foldRight(Exhaustive: Exhaustiveness)(mergeExhaustive)
+  }
+
+  // MATT docs
+  sealed trait Constructors
+
+  object Constructors {
+    def singleton(tycon: TyCon): Constructors = Finite(List(tycon))
+
+    case class Finite(tycons: List[TyCon]) extends Constructors
+    case object Infinite extends Constructors
   }
 }
